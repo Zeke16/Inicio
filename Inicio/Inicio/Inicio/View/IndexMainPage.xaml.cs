31653393a34fbcfcba0097a8698dc6368b6993ac
Centro de Desarrollo De Software <cds@univo.edu.sv>
2019-07-31 11:29:23 -0600

    init

---
+++ Inicio/Inicio/Inicio/View/IndexMainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Inicio.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IndexMainPage : TabbedPage
+    {
+        public IndexMainPage ()
+        {
+            InitializeComponent();
+        }
+    }
+}
